0e3149714bd4e2db7f8aad0f0213b61fa4afe8ff
0s4k4 <soulbosss@gmail.com>
2021-03-04 01:34:46 -0600

    se agrego modelos, y funcionalidad al templeate

--- social/views.py
+++ social/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 #creacion del feed funcion
 def feed(request):
+    #se mandan a llamar todos los post
+    posts = Post.objects.all()
+    #se pasan todos los post con la funcion de context
+    context = {'post': posts}
     return render(request, 'social/feed.html')
 
 #creacion de funcion de perfil
